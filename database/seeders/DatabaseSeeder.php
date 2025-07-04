1e9d07798a47d98d224ffb8e78ca3b1b5802d0d8
nifemiolaniyi4 <nifemiolaniyi4@gmail.com>
2025-07-04 17:09:45 +0000

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
+use App\Models\Job;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Post::factory()->count(50)->create();
+        Job::factory()->count(20)->create();
+        User::factory()->count(10)->create();
     }
 }
