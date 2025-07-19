251ec33e410568c550e226cc3128019f82765723
nifemiolaniyi4 <nifemiolaniyi4@gmail.com>
2025-07-19 05:58:55 +0000

    update

--- resources/views/components/form-field.blade.php
+++ resources/views/components/form-field.blade.php
@@ -8,7 +8,7 @@
             class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 w-full">
             <input
                 {{ $attributes->merge(['class' => 'block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6']) }}
-                name="{{ $fieldname }}" id="{{ $fieldname }}" value="{{ old($fieldname) || $data }}" />
+                name="{{ $fieldname }}" id="{{ $fieldname }}" value="{{ old($fieldname) ?? $data }}" />
         </div>
         {{ $slot }}
     </div>
